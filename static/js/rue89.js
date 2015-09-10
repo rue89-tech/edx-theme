a2a2ecb1e9a0dc09fe16c22a03b57a7d47371cff
mperrien <marc.perrien@gmail.com>
2015-09-10 15:37:19 +0200

    Fixes file organization Completes home and course presentation styling Adds js file for FAQ accordion

---
+++ static/js/rue89.js
@@ -0,0 +1,13 @@
+// listen for the DOMContentLoaded event, then bind our function
+document.addEventListener('DOMContentLoaded', function() {
+	console.log('ready!');
+	$faq = $('.faq');
+	$item = $faq.find('details');
+	$question = $item.find('summary');
+	$answer = $item.children().not('summary');
+	$answer.addClass('answer').hide();
+	$item.on('click', function() {
+		$(this).toggleClass('open');
+		$(this).children().not('summary').toggle(400);
+	});
+});
